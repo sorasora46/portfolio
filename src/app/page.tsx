cfe4fa53e553c952626a7c1aa0124a964aa9e183
Sorrawit Kwanja <sorrawit02546@gmail.com>
2023-10-10 02:50:54 +0700

    💄 add introduction section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 export default function Home() {
   return (
-    <main>Portfolio Page</main>
-  )
+    <main>
+      <section className="standard-dialog introduction">
+        <h2 className="dialog-text intro-header">
+          I am a Full Stack Software Engineer.
+        </h2>
+        <p className="dialog-text intro-description">
+          Hi!, My name is Sorrawit Kwanja, An undergraduate Computer Science
+          student from KMUTT
+        </p>
+      </section>
+    </main>
+  );
 }
