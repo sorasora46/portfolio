2a6ff98f7d1ffb19109d9b081443a6dd291eea79
sorasora46 <sorrawit02546@gmail.com>
2025-03-28 16:50:10 +0700

    💄 add contact, tech stack and styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Sorrawit Kwanja",
-  description: "Sorrawit Kwanja Portfolio",
+  title: "Sorrawit Kwanja | Fullstack Engineer",
+  description: "Portfolio of Sorrawit Kwanja, a Fullstack Engineer passionate about technology and continuous learning.",
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,94 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Hello, world!</h1>
+    <main className="p-3">
+      <div className="sm:w-full md:w-1/3">
+        <div className="flex flex-col gap-1">
+          <span aria-hidden="true" className="w-full border-t-2 border-gray-500"></span>
+          <span aria-hidden="true" className="w-full border-t-2 border-gray-500"></span>
+        </div>
+
+        <h1 className="text-2xl font-bold">
+          <span className="whitespace-nowrap">Sorrawit Kwanja</span>
+          <br />
+          <span className="highlight whitespace-nowrap">Fullstack Engineer</span>
+        </h1>
+
+        <div className="flex flex-col gap-1">
+          <span aria-hidden="true" className="w-full border-t-2 border-gray-500"></span>
+          <span aria-hidden="true" className="w-full border-t-2 border-gray-500"></span>
+        </div>
+
+        <p>Passionate about technology and continuous learning.</p>
+
+        <br />
+
+        <h2 className="highlight">Tech Stack</h2>
+        <div className="flex gap-y-2 gap-x-3 flex-wrap">
+          <div className="chip">
+            <span>Java</span>
+          </div>
+          <div className="chip">
+            <span>Javascript</span>
+          </div>
+          <div className="chip">
+            <span>Typescript</span>
+          </div>
+          <div className="chip">
+            <span>Go</span>
+          </div>
+          <div className="chip">
+            <span>Python</span>
+          </div>
+          <div className="chip">
+            <span>Spring Boot</span>
+          </div>
+          <div className="chip">
+            <span>React.js</span>
+          </div>
+          <div className="chip">
+            <span>Next.js</span>
+          </div>
+          <div className="chip">
+            <span>Express.js</span>
+          </div>
+          <div className="chip">
+            <span>Node.js</span>
+          </div>
+          <div className="chip">
+            <span>Git</span>
+          </div>
+          <div className="chip">
+            <span>Docker</span>
+          </div>
+        </div>
+
+        <br />
+
+        <h2 className="highlight">Contact</h2>
+        <ul>
+          <li>sorrawit.kwanja@gmail.com</li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/sorrawit-kwanja/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500"
+            >
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/sorasora46/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500"
+            >
+              GitHub
+            </a>
+          </li>
+        </ul>
+      </div>
     </main>
   );
 }
