4a38f28be137ecb2c903358c681b6e6c0fa07c16
sorasora46 <sorrawit02546@gmail.com>
2025-03-30 02:58:04 +0700

    💄 edit Projects, ChipList styling

--- src/app/components/ChipList.tsx
+++ src/app/components/ChipList.tsx
@@ -1,12 +1,12 @@
-import { FC } from "react";
+import { FC, HTMLAttributes } from "react";
 
-interface ChipListProps {
-    items: string[]
+interface ChipListProps extends HTMLAttributes<HTMLDivElement> {
+  items: string[]
 }
 
-const ChipList: FC<ChipListProps> = ({ items }) => {
+const ChipList: FC<ChipListProps> = ({ items, className, ...rest }) => {
   return (
-    <div className="flex gap-2 flex-wrap">
+    <div className={`flex gap-2 flex-wrap ${className}`} {...rest}>
       {items.map((item, index) => {
         return (
           <div key={item + index} className="chip">
