2106f28ba7ba697e7133f81f484202613857b2c4
sorasora46 <sorrawit02546@gmail.com>
2025-04-01 18:54:03 +0700

    🚀 set assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  assetPrefix: "https://portfolio.sorrawitk.com/"
 };
 
 export default nextConfig;
